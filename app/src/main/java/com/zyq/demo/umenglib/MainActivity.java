b24cfca64565a90964c5c7d05e98644c95731ed8
Zyq <zhangyanqiang@bus365.com>
2020-11-06 15:15:59 +0800

    初始化项目

--- app/src/main/java/com/zyq/demo/umenglib/MainActivity.java
+++ app/src/main/java/com/zyq/demo/umenglib/MainActivity.java
@@ -1,16 +1,15 @@
 package com.zyq.demo.umenglib;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
